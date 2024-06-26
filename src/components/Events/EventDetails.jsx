0a3b915138a2b1cf948e9e51464029b63ad7b768
Apekshit <apekshit33@gmail.com>
2024-06-26 13:43:39 +0530

    Added delete event functionality

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 import { useMutation, useQuery } from "@tanstack/react-query";
 
 import Header from "../Header.jsx";
 import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import Modal from "../UI/Modal.jsx";
 
 export default function EventDetails() {
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const params = useParams();
   const navigate = useNavigate();
 
@@ -13,15 +18,29 @@ export default function EventDetails() {
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
 
-  const { mutate } = useMutation({
+  const {
+    mutate,
+    isPending: isPendingDeletion,
+    isError: isErrorDeleting,
+    error: deleteError,
+  } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ["events"],
+        refetchType: "none",
       });
       navigate("/events");
     },
   });
+
+  function handleStartDelete() {
+    setIsDeleting(true);
+  }
+
+  function handleStopDelete() {
+    setIsDeleting(false);
+  }
 
   function handleDelete() {
     mutate({ id: params.id });
@@ -59,38 +78,66 @@ export default function EventDetails() {
     });
 
     content = (
-      <div id="event-details-content">
-        <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
-        <div id="event-details-info">
-          <div>
-            <p id="event-details-location">{data.location}</p>
-            <time dateTime={`Todo-DateT$Todo-Time`}>
-              {data.date} @ {data.time}
-            </time>
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClcik={handleStartDelete}>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {data.date} @ {data.time}
+              </time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
           </div>
-          <p id="event-details-description">{data.description}</p>
         </div>
-      </div>
+      </>
     );
   }
 
   return (
     <>
+      {isDeleting && (
+        <Modal>
+          <h2>Are you sure?</h2>
+          <p>
+            Do you really want to delete this event? This action cannot be
+            undone.
+          </p>
+          <div className="form-actions">
+            {isPendingDeletion && <p>Deleting, please wait...</p>}
+            {!isPendingDeletion && (
+              <>
+                <button onClick={handleStopDelete}>Cancel</button>
+                <button onClick={handleDelete}>Delete</button>
+              </>
+            )}
+          </div>
+          {isErrorDeleting && (
+            <ErrorBlock
+              title="Failed to delete event"
+              message={
+                deleteError.info?.message ||
+                "Failed to delete event, please try again later."
+              }
+            />
+          )}
+        </Modal>
+      )}
       <Outlet />
       <Header>
         <Link to="/events" className="nav-item">
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>{data.title}</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-      </article>
+      <article id="event-details">{content}</article>
     </>
   );
 }
